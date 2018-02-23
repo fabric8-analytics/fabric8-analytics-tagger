33d4c9690824e2750775ae182e01ae2b695619cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-23 13:20:34 +0100

    Linter report improvement

--- run-linter.sh
+++ run-linter.sh
@@ -1,6 +1,7 @@
 directories="f8a_tagger"
 separate_files="f8a_tagger_cli.py setup.py"
 
+pass=0
 fail=0
 
 function prepare_venv() {
@@ -33,6 +34,7 @@ do
         if [ $? -eq 0 ]
         then
             echo "    Pass"
+            let "pass++"
         else
             echo "    Fail"
             let "fail++"
@@ -55,6 +57,7 @@ do
     if [ $? -eq 0 ]
     then
         echo "    Pass"
+        let "pass++"
     else
         echo "    Fail"
         let "fail++"
@@ -64,8 +67,9 @@ done
 
 if [ $fail -eq 0 ]
 then
-    echo "All checks passed"
+    echo "All checks passed for $pass source files"
 else
-    echo "Linter fail, $fail source files need to be fixed"
+    let total=$pass+$fail
+    echo "Linter fail, $fail source files out of $total source files need to be fixed"
     exit 1
 fi
