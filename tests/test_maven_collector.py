0bbae1cc6c0237bf0adaa6ec7a53502a308fef43
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-29 10:40:22 +0200

    Unit tests for the Maven collector

--- tests/test_maven_collector.py
+++ tests/test_maven_collector.py
@@ -1,4 +1,34 @@
 """Tests for the MavenCollector class."""
 
 import pytest
+from os import path, remove
 from f8a_tagger.collectors.maven import MavenCollector
+from f8a_tagger.errors import InstallPrepareError
+from f8a_tagger.utils import get_files_dir
+
+
+def test_initial_state():
+    """Check the initial state of the class."""
+    c = MavenCollector()
+    assert c is not None
+
+
+def test_execute_method():
+    """Test the execute() method."""
+    c = MavenCollector()
+
+    # make sure the maven-index-checker is not present!
+    filedir = get_files_dir()
+    maven_index_checker_jar = path.join(filedir, "maven-index-checker.jar")
+    try:
+        remove(maven_index_checker_jar)
+    except FileNotFoundError as e:
+        pass
+
+    with pytest.raises(InstallPrepareError) as e:
+        c.execute()
+
+
+if __name__ == '__main__':
+    test_initial_state()
+    test_execute_method()
