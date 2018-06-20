a29f3172cb3faad25b1fa1304ba8ff14fdfa67d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-20 13:49:10 +0200

    Fixed remaining shellcheck-related issues

--- runtests.sh
+++ runtests.sh
@@ -2,17 +2,35 @@
 
 COVERAGE_THRESHOLD=90
 
-export PYTHONPATH=`pwd`/f8a_tagger/
+export TERM=xterm
+TERM=${TERM:-xterm}
 
-echo "Create Virtualenv for Python deps ..."
+# set up terminal colors
+NORMAL=$(tput sgr0)
+RED=$(tput bold && tput setaf 1)
+GREEN=$(tput bold && tput setaf 2)
+YELLOW=$(tput bold && tput setaf 3)
+
+PYTHONPATH=$(pwd)/f8a_tagger/
+export PYTHONPATH
+
+printf "%sCreate Virtualenv for Python deps ..." "${NORMAL}"
+
 function prepare_venv() {
-    VIRTUALENV=`which virtualenv`
-    if [ $? -eq 1 ]; then
+    VIRTUALENV=$(which virtualenv)
+    if [ $? -eq 1 ]
+    then
         # python34 which is in CentOS does not have virtualenv binary
-        VIRTUALENV=`which virtualenv-3`
+        VIRTUALENV=$(which virtualenv-3)
     fi
 
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt && python3 `which pip3` install -r test_requirements.txt
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt && python3 "$(which pip3)" install -r test_requirements.txt
+    if [ $? -ne 0 ]
+    then
+        printf "%sPython virtual environment can't be initialized%s" "${RED}" "${NORMAL}"
+        exit 1
+    fi
+    printf "%sPython virtual environment initialized%s\n" "${YELLOW}" "${NORMAL}"
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
@@ -30,5 +48,6 @@ radon mi -s -i venv .
 echo "*****************************************"
 echo "*** Unit tests ***"
 echo "*****************************************"
-cd tests
-PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` --cov=../f8a_tagger/ --cov-report term-missing --cov-report xml --cov-fail-under=$COVERAGE_THRESHOLD -vv .
+cd tests || exit
+PYTHONDONTWRITEBYTECODE=1 python3 "$(which pytest)" --cov=../f8a_tagger/ --cov-report term-missing --cov-report xml --cov-fail-under=$COVERAGE_THRESHOLD -vv .
+printf "%stests passed%s\n\n" "${GREEN}" "${NORMAL}"
