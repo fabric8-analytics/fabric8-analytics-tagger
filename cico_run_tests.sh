99d2d8007cb4bc27de42a09537cf0da011970fd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-20 13:58:58 +0100

    Install Git

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -6,7 +6,7 @@ prep() {
     yum -y update
     yum -y install epel-release
     yum -y install python34 python34-virtualenv which libarchive
-    yum -y install gcc
+    yum -y install gcc git
 }
 
 prep
