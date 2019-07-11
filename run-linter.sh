4a0da10bd0fa244a4baea2be5d6e06a1fa7f4608
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-11 10:26:21 +0200

    Update the script run-linter.sh to use directories.txt

--- run-linter.sh
+++ run-linter.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-directories="f8a_tagger tests tools"
-separate_files="f8a_tagger_cli.py setup.py"
+IFS=$'\n'
+
+# list of directories with sources to check
+directories=$(cat directories.txt)
+
+# list of separate files to check
+separate_files=$(cat files.txt)
 
 pass=0
 fail=0
@@ -9,7 +14,7 @@ fail=0
 function prepare_venv() {
     VIRTUALENV=$(which virtualenv)
     if [ $? -eq 1 ]; then
-        # python34 which is in CentOS does not have virtualenv binary
+        # python36 which is in CentOS does not have virtualenv binary
         VIRTUALENV=$(which virtualenv-3)
     fi
 
